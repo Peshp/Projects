5d60fed0fbdaeb02a985f846d15db71dcf2af7fd
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-12 18:12:02 +0300

    Add files via upload

---
+++ Tic Tac Toe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            resources.ApplyResources(this.newGameToolStripMenuItem, "newGameToolStripMenuItem");
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            resources.ApplyResources(this.infoToolStripMenuItem, "infoToolStripMenuItem");
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.b9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b1, 0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // b9
+            // 
+            this.b9.FlatAppearance.BorderSize = 5;
+            resources.ApplyResources(this.b9, "b9");
+            this.b9.Name = "b9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b8
+            // 
+            this.b8.FlatAppearance.BorderSize = 5;
+            resources.ApplyResources(this.b8, "b8");
+            this.b8.Name = "b8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b7
+            // 
+            this.b7.FlatAppearance.BorderSize = 5;
+            resources.ApplyResources(this.b7, "b7");
+            this.b7.Name = "b7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b6
+            // 
+            this.b6.FlatAppearance.BorderSize = 5;
+            resources.ApplyResources(this.b6, "b6");
+            this.b6.Name = "b6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b5
+            // 
+            this.b5.FlatAppearance.BorderSize = 5;
+            resources.ApplyResources(this.b5, "b5");
+            this.b5.Name = "b5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b4
+            // 
+            this.b4.FlatAppearance.BorderSize = 5;
+            resources.ApplyResources(this.b4, "b4");
+            this.b4.Name = "b4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b3
+            // 
+            this.b3.FlatAppearance.BorderSize = 5;
+            resources.ApplyResources(this.b3, "b3");
+            this.b3.Name = "b3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.FlatAppearance.BorderSize = 5;
+            resources.ApplyResources(this.b2, "b2");
+            this.b2.Name = "b2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b1
+            // 
+            this.b1.FlatAppearance.BorderSize = 5;
+            resources.ApplyResources(this.b1, "b1");
+            this.b1.Name = "b1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+    }
+}
+
---
+++ Tic Tac Toe/TicTacToe/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        char o = 'O';
+        int movement = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            o = 'o';
+            movement = 0;
+            b1.Enabled = true; b1.Text = "";
+            b2.Enabled = true; b2.Text = "";
+            b3.Enabled = true; b3.Text = "";
+            b4.Enabled = true; b4.Text = "";
+            b5.Enabled = true; b5.Text = "";
+            b6.Enabled = true; b6.Text = "";
+            b7.Enabled = true; b7.Text = "";
+            b8.Enabled = true; b8.Text = "";
+            b9.Enabled = true; b9.Text = "";
+            tableLayoutPanel1.Enabled = true;
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            Button bt = sender as Button;
+            bt.Enabled = false;
+            bt.BackColor = Color.White;
+            if(o == 'O')
+            {
+                bt.Text = "O";
+                if((b1.Text == b2.Text && b2.Text == b3.Text && b2.Text != "") ||
+                        (b4.Text == b5.Text && b5.Text == b6.Text && b5.Text != "") ||
+                        (b7.Text == b8.Text && b8.Text == b9.Text && b8.Text != "") ||
+                        (b1.Text == b4.Text && b4.Text == b7.Text && b4.Text != "") ||
+                        (b2.Text == b5.Text && b5.Text == b8.Text && b5.Text != "") ||
+                        (b3.Text == b6.Text && b6.Text == b9.Text && b6.Text != "") ||
+                        (b1.Text == b5.Text && b5.Text == b9.Text && b5.Text != "") ||
+                        (b3.Text == b5.Text) && (b5.Text == b7.Text) && (b5.Text != ""))
+                {
+                    MessageBox.Show($"The winner is {o.ToString().ToUpper() }!");
+                    tableLayoutPanel1.Enabled = false;
+                }
+                else if(movement == 8)
+                {
+                    MessageBox.Show("Draw!");
+                }
+
+                o = 'X';
+            }
+            else if(o == 'X')
+            {
+                bt.Text = "X";
+                if ((b1.Text == b2.Text && b2.Text == b3.Text && b2.Text != "") ||
+                        (b4.Text == b5.Text && b5.Text == b6.Text && b5.Text != "") ||
+                        (b7.Text == b8.Text && b8.Text == b9.Text && b8.Text != "") ||
+                        (b1.Text == b4.Text && b4.Text == b7.Text && b4.Text != "") ||
+                        (b2.Text == b5.Text && b5.Text == b8.Text && b5.Text != "") ||
+                        (b3.Text == b6.Text && b6.Text == b9.Text && b6.Text != "") ||
+                        (b1.Text == b5.Text && b5.Text == b9.Text && b5.Text != "") ||
+                        (b3.Text == b5.Text) && (b5.Text == b7.Text) && (b5.Text != ""))
+                {
+                    MessageBox.Show($"The winner is {o.ToString().ToUpper() }!");
+                    tableLayoutPanel1.Enabled = false;
+                }
+                else if (movement == 8)
+                {
+                    MessageBox.Show("Draw!");
+                }
+
+                o = 'O';
+            }
+            movement++;
+        }      
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is Tic Tac Toe game, made on Visual Studio 2019 with C$ language by Anton (Peshp).");
+        }
+    }
+}
